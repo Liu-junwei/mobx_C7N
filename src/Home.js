c21daf3653c35b75e0c2df4e1a8444fdfe747949
Liu-junwei <2546743842@qq.com>
2021-04-02 15:25:30 +0800

    first commit

--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,17 @@
-
+import './App.css'
 import 'choerodon-ui/dist/choerodon-ui.css';
 import 'choerodon-ui/dist/choerodon-ui-pro.css';
 import 'choerodon-ui/dist/choerodon-ui-demo-data-mock.min.js';
 import React from 'react';
+import { inject, observer } from 'mobx-react'
 
-import { Table, Input, Popconfirm } from 'choerodon-ui';
+import { Table, Input, Popconfirm, Button } from 'choerodon-ui';
 
 
 const EditableCell = ({ editable, value, onChange }) => (
     <div>
         {editable
-            ? <Input style={{ margin: '-5px 0' }} value={value} onChange={e => onChange(e.target.value)} />
+            ? <Input className='editInput' value={value} onChange={e => onChange(e.target.value)} />
             : value
         }
     </div>
@@ -23,26 +24,32 @@ class EditableTable extends React.Component {
 
         super(props);
         const { store } = this.props
-        this.state = { store }
+        let cacheData = store.todos.map(item => ({ ...item }));
+        this.state = { store, cacheData }
 
         this.columns = [{
-            title: 'name',
+            title: '姓名',
+            align: 'center',
             dataIndex: 'name',
             width: '25%',
             render: (text, record) => this.renderColumns(text, record, 'name'),
         }, {
-            title: 'age',
+            title: '年龄',
             dataIndex: 'age',
+            align: 'center',
             width: '15%',
             render: (text, record) => this.renderColumns(text, record, 'age'),
         }, {
-            title: 'address',
+            title: '地址',
+            align: 'center',
             dataIndex: 'address',
-            width: '40%',
+            width: '30%',
             render: (text, record) => this.renderColumns(text, record, 'address'),
         }, {
-            title: 'operation',
+            title: '操作',
+            width: '40%',
             dataIndex: 'operation',
+            align: 'center',
             render: (text, record) => {
                 const { editable } = record;
                 return (
@@ -50,20 +57,21 @@ class EditableTable extends React.Component {
                         {
                             editable ? (
                                 <span>
-                                    <a onClick={() => this.save(record.key)}>Save</a>
-                                    <Popconfirm title="Sure to cancel?" onConfirm={() => this.cancel(record.key)}>
-                                        <a>Cancel</a>
-                                    </Popconfirm>
+                                    <Button type="primary" onClick={() => this.save(record.key)}>保存</Button>
                                 </span>
                             )
-                                : <a onClick={() => this.edit(record.key)}>Edit</a>
+                                : <Button type="primary" onClick={() => this.edit(record.key)}>编辑</Button>
                         }
+                        <Popconfirm title="确认删除吗?" onConfirm={() => this.delete(record.key)}>
+                            <Button type="primary">删除</Button>
+                        </Popconfirm>
+
                     </div>
                 );
             },
         }];
 
-        this.cacheData = store.todos.map(item => ({ ...item }));
+
     }
 
     renderColumns(text, record, column) {
@@ -77,45 +85,134 @@ class EditableTable extends React.Component {
     }
 
     handleChange(value, key, column) {
-        const newData = [...this.state.store];
+        const newData = [...this.state.cacheData];
+        const news = [...this.state.store.todos]
         const target = newData.filter(item => key === item.key)[0];
+        const targets = news.filter(item => key === item.key)[0];
         if (target) {
             target[column] = value;
-            this.state.store.updata(newData)
+            this.setState({ cacheData: newData })
         }
+        if (targets) {
+            targets[column] = value;
+            this.state.store.updata(news)
+        }
+
     }
 
     edit(key) {
-        const newData = [...this.state.store];
+        const newData = [...this.state.cacheData];
         const target = newData.filter(item => key === item.key)[0];
+        const targets = newData.filter(item => item.editable === true)[0];
+        if (targets) {
+            alert('请先保存未保存项！')
+            return false
+        }
         if (target) {
             target.editable = true;
-            this.state.store.updata(newData)
+            this.setState({ cacheData: newData })
         }
     }
 
     save(key) {
-        const newData = [...this.state.store];
+        const newData = [...this.state.cacheData];
+        const newDatas = [...this.state.store.todos];
         const target = newData.filter(item => key === item.key)[0];
+        const targets = newDatas.filter(item => key === item.key)[0];
         if (target) {
             delete target.editable;
-            this.state.store.updata(newData)
-            this.cacheData = newData.map(item => ({ ...item }));
+            delete targets.editable;
+
+            this.setState({ cacheData: newData })
         }
     }
 
-    cancel(key) {
-        const newData = [...this.state.store];
-        const target = newData.filter(item => key === item.key)[0];
-        if (target) {
-            Object.assign(target, this.cacheData.filter(item => key === item.key)[0]);
-            delete target.editable;
-            this.state.store.updata(newData)
+    delete(key) {
+        const newData = [...this.state.cacheData];
+        const news = [...this.state.store.todos];
+
+        for (let i = 0; i < newData.length; i++) {
+            if (newData[i].key === key) {
+                newData.splice(i, 1)
+                break;
+            }
         }
+        for (let j = 0; j < news.length; j++) {
+            if (news[j].key === key) {
+                news.splice(j, 1)
+                break;
+            }
+        }
+        this.setState({ cacheData: newData })
+        this.state.store.updata(news)
+
+
+    }
+    add(key) {
+        const newData = [...this.state.cacheData];
+        const news = [...this.state.store.todos];
+        let keys = news[news.length - 1].key + 1
+
+        newData.push({ key: keys, name: '', age: '', address: '' })
+        newData[newData.length - 2].editable = false
+        newData[newData.length - 1].editable = true
+
+        news.push({ key: keys, name: "", age: '', address: '' })
+        news[news.length - 2].editable = false
+        news[news.length - 1].editable = true
+
+        this.setState({ cacheData: newData })
+        this.state.store.updata(news)
+
     }
 
+    changeSearch(e) {
+        this.state.store.changeSearch(e)
+    }
+    clickSearch() {
+        let newData = [...this.state.store.todos];
+        let searchName = this.state.store.searchValue
+
+        let reg = new RegExp(searchName);
+        newData = newData.filter((item) => { return reg.test(item.name) });
+        this.setState({ cacheData: newData })
+
+    }
+   
     render() {
-        return <Table bordered dataSource={this.state.store} columns={this.columns} />;
+
+        return (
+            <div>
+                <div style={{ margin: '10px 5px' }}>
+                    <Input
+                        style={{ width: "150px" }}
+                        value={this.state.store.searchValue}
+                        onChange={this.changeSearch.bind(this)}
+                        placeholder='请输入姓名'
+                    />
+                    <Button
+                        style={{ marginRight: '20px' }}
+                        shape="circle"
+                        funcType="flat"
+                        icon="search"
+
+                        onClick={this.clickSearch.bind(this)}
+                    />
+                    <Button onClick={() => this.add()} funcType="raised">新增成员</Button>
+
+                </div>
+
+                <Table
+                    style={{ width: '500px' }}
+                    filterBar={false}
+                    bordered
+                    dataSource={this.state.cacheData}
+                    columns={this.columns}
+
+                />
+            </div>
+
+        )
     }
 }
 export default EditableTable;
--- src/store.js
+++ src/store.js
@@ -1,24 +1,59 @@
-import { action, observable, computed } from 'mobx'
+
+
+import { action, observable } from 'mobx'
 import moment from 'moment'
 
-class AppStore {
 
-    @observable time = '2019'
+class AppStore {
+   
+    @observable searchValue = ''
     @observable todos = [{
         key: '1',
-        name: '胡彦斌',
+        name: '周杰伦',
         age: 32,
-        address: '西湖区湖底公园1号'
-
-    }, {
+        address: '东方明珠外滩1号'
+  
+    },
+        {
         key: '2',
-        name: '胡彦祖',
+            name: '吴彦祖',
+            age: 22,
+            address: '西湖区湖底公园1号'
+        },
+        {
+            key: '3',
+            name: '张杰',
+            age: 42,
+            address: '岳麓区五一大药房'
+        },
+        {
+            key: '4',
+            name: '范冰冰',
+            age: 43,
+            address: '广州市白云区白云街道'
+        },
+        {
+            key: '5',
+            name: '陈奕迅',
+            age: 42,
+            address: '西湖区大炮台公园3号'
+        },
+        {
+            key: '6',
+            name: '李连杰',
+            age: 42,
+            address: '东风区火云街道'
+        },
+        {
+            key: '7',
+            name: '张含韵',
         age: 42,
-        address: '西湖区湖底公园1号'
-    }];
-
-    @computed get desc() {
-        return `${this.time}还有${this.todos.length}条任务待完成`
+            address: '成昌区百运大道88号'
+        }
+    ]
+   
+    @action changeSearch(e) {
+        this.searchValue = e.target.value
     }
     @action updata(arr) {
         this.todos = arr
